3d55eaf7d19d6c2348f52d1b3ef56c27ee464ea9
Enrique Pereira <enrique.pereira.ssp@gmail.com>
2023-06-07 12:09:07 -0300

    Add ag config

--- after/plugin/ag.lua
+++ after/plugin/ag.lua
@@ -1,11 +1,11 @@
 local Terminal = require("toggleterm.terminal").Terminal
 
-local new_cmd_client = function (cmd)
+local new_cmd_client = function (cmd, dir, direction)
     return Terminal:new {
         cmd = cmd,
-        dir = "./",
+        dir = dir,
         hidden = true,
-        direction = "float",
+        direction = direction,
         close_on_exit = false,
         float_opts = {
             border = "curved",
@@ -14,12 +14,22 @@ local new_cmd_client = function (cmd)
     }
 end
 
+vim.keymap.set("n", "<leader>kd", ":!kubectx dev && make rollout-dev<CR>")
+vim.keymap.set("n", "<leader>kl", ":!kubectx local && make rollout-local<CR>")
+
 vim.keymap.set("n", "<leader>mr", ":!make run<CR>")
-vim.keymap.set("n", "<leader>md", ":!make rollout-dev<CR>")
-vim.keymap.set("n", "<leader>ml", ":!make rollout-local<CR>")
-vim.keymap.set("n", "<leader>mx", function ()
-    new_cmd_client("make run-debug"):toggle()
+vim.keymap.set("n", "<leader>md", function ()
+    new_cmd_client("make run-debug", "./", "float"):toggle()
 end)
 
 
+vim.keymap.set("n", "<leader>cb", function ()
+    new_cmd_client("make run-booking-spec", "~/projects/ag/poc-cypressio", "vertical"):toggle()
+end);
+vim.keymap.set("n", "<leader>ct", function ()
+    new_cmd_client("make run-ticketing-spec", "~/projects/ag/poc-cypressio", "vertical"):toggle()
+end);
+vim.keymap.set("n", "<leader>ci", function ()
+    new_cmd_client("make run-import-booking-spec", "~/projects/ag/poc-cypressio", "vertical"):toggle()
+end);
 
